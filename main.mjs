836e90c9b377a47b7bfde5e9060b14904e5082d4
NooGat <kinghyde111@gmail.com>
2021-01-09 11:22:30 +0100

    settle presentation

--- main.mjs
+++ main.mjs
@@ -2,10 +2,11 @@ import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 import "reveal.js/dist/reveal.css";
-import 'reveal.js/dist/theme/black.css';
+import 'reveal.js/dist/theme/moon.css';
 
 let deck = new Reveal({
    slideNumber: true,
+   controls: false,
    plugins: [ Markdown, Notes ]
 });
 
